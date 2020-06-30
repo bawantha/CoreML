45951948fd98cf6e8eefcdfd894907c762f935ec
Bawantha <rajapakshabawantha@gmail.com>
2020-06-30 17:47:55 +0530

    Initial Commit

---
+++ CoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML
+//
+//  Created by user154135 on 6/30/20.
+//  Copyright © 2020 bawantha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
